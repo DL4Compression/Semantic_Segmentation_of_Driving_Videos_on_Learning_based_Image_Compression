f96a66a16fbcc0f2040055a09f512ea3e90b7d3a
PragyadiptaAdhya <118466922+PragyadiptaAdhya@users.noreply.github.com>
2023-07-04 23:05:00 +0530

    Add files via upload

---
+++ src/Compression/training_and_inference/compression_training.py
@@ -0,0 +1,233 @@
+import torch, torchvision, os
+from torch.utils import data
+from torch.backends import cudnn
+import functools, operator, numpy as np
+from termcolor import colored
+import json
+cudnn.benchmark = True
+from utils.compression_train_dataset import IDD
+from model_without_batchnorm import autoencoder
+from torchmetrics import PeakSignalNoiseRatio
+# from evaluators import compare_psnr_batch, compare_ssim_batch
+import wandb
+wandb.init(project="CAE_natural_video_compression_IDD_driving_16_03_23",name = "model_d_2", entity="ravijk")
+from tqdm import tqdm as tq
+import random
+
+
+# For Reproducability
+random.seed(3407)
+np.random.seed(3407)
+torch.manual_seed(3407)
+torch.cuda.manual_seed(3407)
+torch.cuda.manual_seed_all(3407)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+
+def main( args ):
+	images_transforms = torchvision.transforms.Compose([ torchvision.transforms.ToTensor()])
+	images_transforms_test = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+	iddtrain = IDD(args.traindata,
+		transform_images=images_transforms)
+	iddtraindl = data.DataLoader(iddtrain, batch_size=args.batch_size, num_workers=8, pin_memory=True, shuffle=True)
+	iddtest = IDD(args.testdata,
+		transform_images=images_transforms_test)
+	iddtestdl = data.DataLoader(iddtest, batch_size=args.batch_size, num_workers=8, pin_memory=True, shuffle=False)
+
+	# Model instance with variable number of DownConv blocks
+	# args.n_downconvs = 3 corresponds to CVPR paper
+	model = autoencoder(args.n_convblocks)
+	if torch.cuda.is_available() and args.gpu:
+		model = model.cuda()
+	optimizer = torch.optim.Adam(model.parameters()) # usual optimizer instance
+	schedular = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.75) # schedular instance
+	psnr_loss = PeakSignalNoiseRatio().cuda()
+	# Resume from model
+	start_epoch = 0
+	# load model file (if present) and load the model,optimizer & schedular states
+	model_file = '.'.join([args.model_file_name, 'model'])
+	if os.path.exists(os.path.abspath(model_file)):
+		loaded_file = torch.load(os.path.abspath(model_file))
+		model.load_state_dict(loaded_file['model_state'])
+		optimizer.load_state_dict(loaded_file['optim_state'])
+		schedular.load_state_dict(loaded_file['schedular_state'])
+		start_epoch = loaded_file['epoch'] + 1
+	
+		print('{tag} resuming from saved model'.format(tag=colored('[Saving]', 'red')));
+		del loaded_file
+	
+	# For logging purpose;
+	logg = [] 
+	log_file = '.'.join([args.model_file_name, 'log'])
+	if os.path.exists(os.path.abspath(log_file)):
+		# if .log file exists, open it
+		with open(log_file, 'r') as logfile:
+			logg = json.load(logfile)
+	
+	msecrit = torch.nn.MSELoss() # usual MSE loss function
+
+	# prev_test_ssim = -np.inf
+	prev_test_psnr = -np.inf
+	for epoch in range(start_epoch, args.epochs):
+		# regularly invoke the schedular
+
+		print(f'Epoch:{epoch}/{args.epochs}')
+
+		# choose randomize subset if requested
+		if args.randomize_subset:
+			iddtrain.choose_random_subset(args.subset_size)
+
+		model.train() # training mode ON
+		cnt=0
+		avg_train_loss = 0
+		avg_train_ssim = 0
+		avg_train_psnr = 0
+
+		print("Training Loop!")
+
+		for idx, images in enumerate(tq(iddtraindl)):
+			# The data fetch loop
+			if torch.cuda.is_available() and args.gpu:
+				images = images.cuda()
+
+			optimizer.zero_grad() # zero out grads
+			output = model(images) # forward pass
+
+			loss = msecrit(output, images)
+			# psnr = psnr_loss(images,output)
+			# psnr = 20.0 * torch.log10(psnr).item()
+			# compute the required metrics
+			# if epoch%10==0:
+			# 	ssim = compare_ssim_batch(labels.detach().cpu().numpy(), output.detach().cpu().numpy())
+			# 	psnr = compare_psnr_batch(labels.detach().cpu().numpy(), output.detach().cpu().numpy())
+				# psnr = 20.0 * np.log10(psnr)
+			avg_train_loss += loss
+			# avg_train_psnr += psnr
+			# 	avg_train_ssim = avg_train_ssim+ssim
+			# 	cnt = cnt+1
+
+				# if idx % args.interval == 0:
+				# 	print('{tag} {0:4d}/{1:4d}/{2:4d} -> Loss: {3:.8f}, pSNR: {4:.8f}dB, SSIM: {5:.8f}'.format(
+				# 		idx, epoch, args.epochs, loss.item(), psnr, ssim, tag=colored('[Training]','yellow')))
+
+			loss.backward() # backward
+			optimizer.step() # weight updatepr
+			# print(idx)
+		# if epoch%10==0:
+		avg_train_loss = avg_train_loss/idx+1
+		# avg_train_psnr = avg_train_psnr/idx+1
+		# 	avg_train_ssim = avg_train_ssim/cnt
+
+		wandb.log(			
+			{
+				"epochs": epoch,
+				"Training mse loss": avg_train_loss,
+				# "Training pSNR": avg_train_psnr,
+				# "Training SSIM": avg_train_ssim,
+			}
+		)
+		# TRAINING DONE
+		model.eval() # switch to evaluation mode
+
+		n = 0
+		avg_loss, avg_ssim, avg_psnr = 0.0, 0.0, 0.0
+		with torch.no_grad():
+			print("Validation Loop")
+			# Testing phase starts
+			for idx, (images) in enumerate(tq(iddtestdl)):
+				if torch.cuda.is_available() and args.gpu:
+					images = images.cuda()
+				# print('image shape:',images.shape)
+				# print('label_shape:',labels.shape)
+				output = model(images) # forward pass
+				# print('output shape:',output.shape)
+				loss = msecrit(output, images) # loss calculation
+				# calculate the metrics (SSIM and pSNR)
+				psnr = psnr_loss(images,output).item()
+				# ssim = compare_ssim_batch(labels.detach().cpu().numpy(), output.detach().cpu().numpy())
+				# psnr = compare_psnr_batch(labels.detach().cpu().numpy(), output.detach().cpu().numpy())
+
+				avg_loss = ((n * avg_loss) + loss.item()) / (n + 1) # running mean
+				# avg_ssim = ((n * avg_ssim) + ssim) / (n + 1) # running mean
+				avg_psnr = ((n * avg_psnr) + psnr) / (n + 1) # running mean
+				n += 1
+
+		avg_psnr = 20.0 * np.log10(avg_psnr) # convert pSNR to dB
+
+		schedular.step()
+
+		# print('{tag} Epoch: {0:3d}, Loss: {3:.8f}, pSNR: {1:.8f}, SSIM: {2:.8f}'.format(
+		# 			epoch, avg_psnr, avg_ssim, avg_loss, tag=colored('[Testing]','cyan')))
+					
+		wandb.log(			
+			{
+				"epoch": epoch,
+				"validation loss": avg_loss,
+				"validation pSNR": avg_psnr,
+				# "validation SSIM": avg_ssim,
+			}
+		)
+		# wandb.watch(model)
+		logg.append(
+			{
+				'epoch': epoch,
+				'loss': avg_loss,
+				'pSNR': avg_psnr,
+				# 'SSIM': avg_ssim,
+				'lr': optimizer.param_groups[0]['lr']
+			}
+		) # accumulate information for the .log file
+
+		# name of the log file
+		with open(log_file, 'w') as logfile:
+			json.dump(logg, logfile)
+
+		# model saving, only if the SSIM is better than before
+		# if avg_ssim > prev_test_ssim:
+		if avg_psnr > prev_test_psnr:
+			print(colored('[Saving] model saved to {}'.format(model_file), 'red'))
+			torch.save({
+				'epoch': epoch,
+				'model_state': model.state_dict(),
+				'optim_state': optimizer.state_dict(),
+				'schedular_state': schedular.state_dict(),
+				}, os.path.abspath(model_file))
+			# prev_test_ssim = avg_ssim
+			prev_test_psnr = avg_psnr
+
+		else:
+			print(colored('[Saving] model NOT saved'))
+
+if __name__ == '__main__':
+
+	import argparse
+
+	parser = argparse.ArgumentParser(
+	"""
+	Training script.
+
+	Provide the folder paths which contains traning (patch) images and testing (full-scale mammogram) images produced by dataprep.py.
+	Provide a filename prefix to help differentiate model/log from different runs with different configurations.
+
+	(Optionally) You may choose to provide batch size (--batch_size), number of epochs (--epoch), printing interval (--interval).
+	(Optionally) You may choose to limit the number of samples (--subset_size) to be used per epoch. '0' means the whole dataset.
+	(Optionally) You may also choose to randomize (--randomize_subset) the selected subset every epoch. It has effect only if --subset_size is non-zero.
+	(Optionally) You may choose to use GPU (--gpu).
+	(Optionally) You may choose the number of Convolution blocks in the architecture. (CVPR paper have 3)
+	"""
+	)
+	parser.add_argument('--traindata', type=str, required=True, help='Folder path of the cropped images (train)')
+	parser.add_argument('--testdata', type=str, required=True, help='Folder path to the full-scale mammogram images (test)')
+	parser.add_argument('-b', '--batch_size', type=int, required=False, default=32, help='Batch size')
+	parser.add_argument('-e', '--epochs', type=int, required=False, default=100, help='Number of epochs to run')
+	parser.add_argument('-g', '--gpu', action='store_true', help='Want GPU ?')
+	parser.add_argument('-i', '--interval', type=int, required=False, default=50, help='Iteration interval for display')
+	parser.add_argument('-s', '--subset_size', type=int, required=False, default=0, help='Size of the randomly selected subset')
+	parser.add_argument('--model_file_name', type=str, required=True, help='Name of model and log files')
+	parser.add_argument('--randomize_subset', action='store_true', help='Randomize selected subset in every epoch ?')
+	parser.add_argument('--n_convblocks', type=int, required=False, default=3, help='no of conv blocks in the architecture')
+	
+	args = parser.parse_args()
+
+	main( args )
---
+++ src/Compression/training_and_inference/infer_save_lat_decom.py
@@ -0,0 +1,232 @@
+from bz2 import compress
+import torch, torchvision, os
+from torch.utils import data
+from torch.backends import cudnn
+cudnn.benchmark = True
+import torch.nn as nn
+import numpy as np
+import sys
+import pickle, pdb
+from PIL import Image
+import huffman
+from dahuffman import HuffmanCodec
+import cv2
+import time
+from tqdm import tqdm as tq
+from torchmetrics.functional import structural_similarity_index_measure as ssim
+from torchmetrics.functional import peak_signal_noise_ratio as psnr
+
+from model_without_batchnorm import autoencoder
+from utils.compression_test_dataset import IDDCOM
+import timeit
+
+# The Float->Int module
+class Float2Int(nn.Module):
+    def __init__(self, bit_depth=8):
+        super().__init__()
+        self.bit_depth = bit_depth
+
+    def forward(self, x):
+        x = torch.round(x * (2**self.bit_depth - 1)).type(torch.int32)
+        return x
+
+# The Int->Float module
+class Int2Float(nn.Module):
+    def __init__(self, bit_depth=8):
+        super().__init__()
+        self.bit_depth = bit_depth
+
+    def forward(self, x):
+        x = x.type(torch.float32) / (2**self.bit_depth - 1)
+        return x
+
+def main( args ):
+    # module instances
+    device = torch.device('cuda' if (torch.cuda.is_available() and args.gpu) else 'cpu')
+
+    model = autoencoder(args.n_convblocks)
+    float2int = Float2Int(args.bit_depth)
+    int2float = Int2Float(args.bit_depth)
+
+    # GPU transfer
+    
+
+    # load the given model
+    # with open(os.path.abspath(), 'rb') as modelfile:
+    
+    loaded_model_file = torch.load(args.model_file)
+    model.load_state_dict(loaded_model_file['model_state'])
+    # ssim = StructuralSimilarityIndexMeasure().cuda()
+    # psnr = PeakSignalNoiseRatio().cuda()
+    if torch.cuda.is_available() and args.gpu:
+        # print("model sent to cuda")
+        model = model.to(device)
+        float2int, int2float = float2int.to(device), int2float.to(device)
+    
+    images_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+    labels_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+    avg_cf, avg_ssim, avg_psnr,avg_bits = 0., 0., 0., 0.
+    n = 0
+    
+    for idx,dirc in enumerate(tq(os.listdir(args.inferdata))):
+        
+        bit_rate_raw = []
+        bit_rate_cae = []
+        inf_path = os.path.join(args.inferdata,dirc)
+        iddinfer = IDDCOM(inf_path, transform_images=images_transforms, transform_masks=labels_transforms, preserve_names=True)
+        iddinferdl = data.DataLoader(iddinfer, batch_size=1, num_workers=4, pin_memory=True, shuffle=False)
+
+        model.eval() # switch to evaluation mode
+        with torch.no_grad():
+            # a global counter & accumulation variables
+            
+            all_cf = []
+            all_bits = []
+            
+            for idx, (image, _, name) in enumerate(tq(iddinferdl)):
+                img_size = sys.getsizeof(image.storage())
+                if torch.cuda.is_available() and args.gpu:
+                    # print("img sent to cuda")
+                    image = image.to(device)
+                # print(torch.max(image)
+                # print(image.)
+                start = timeit.timeit()
+                compressed = model.encoder(image) # forward through encoder
+                end = timeit.timeit()
+                # print(end - start)
+                latent_int = float2int(compressed) # forward through Float2Int module
+
+                # usual numpy conversions
+                image_numpy = image.cpu().numpy()
+                latent_int_numpy = latent_int.cpu().numpy()
+
+                
+                if args.with_aac:
+                    # encode latent_int with Huffman coding
+                    inpt=[]
+                    c1, c, h, w = latent_int_numpy.shape
+                    flat = latent_int_numpy.flatten()
+                    for i in flat:
+                        inpt.append(str(i))
+                    codec = HuffmanCodec.from_data(inpt)
+                    encoded = codec.encode(inpt)
+                    hufbook = codec.get_code_table()
+                    book_size = sys.getsizeof(hufbook)
+                    code_size = sys.getsizeof(encoded)
+                    
+                    bit_rate_raw.append(img_size*(30/1.8))
+                    bit_rate_cae.append((book_size+code_size)*(30/1.8))
+
+                    cf = img_size/(book_size+code_size)
+                    all_cf.append(cf)
+                    bits_al = []
+                    for symbol, (bits, val) in hufbook.items():
+                        bits_al.append(bits)
+                    bits_al = np.array(bits_al)
+                    av_bits = np.mean(bits_al)
+                    all_bits.append(av_bits)
+                    decoded = codec.decode(encoded)
+                    ar_in=[]
+                    for i in decoded:
+                        ar_in.append(int(i))
+                    ar_in = np.array(ar_in)
+                    latent = ar_in.reshape([c1,c,h,w])
+                    latent_inp = torch.from_numpy(latent).cuda()
+                else:
+                    bits = args.bit_depth
+                    Q = None
+                # st= timeit.timeit()
+                latent_float = int2float(latent_inp) # back to float
+                decompressed = model.decoder(latent_float) # forward through decoder
+
+                # avg_ssim += ssim(image,decompressed).item()
+                # avg_psnr += psnr(image,decompressed).item()
+
+                original, reconstructed = image_numpy, decompressed.cpu().numpy()
+                # en = timeit.timeit()
+                # print(en-st)
+                all_cf.append(cf)
+
+                n += 1
+
+                if args.produce_latent_code:
+                    # save the latent code if requested. the saved items are
+                    if not(os.path.exists(os.path.join(args.out_latent,dirc))):
+                        os.mkdir(os.path.join(args.out_latent,dirc))
+                    # np.save(os.path.join(args.out_latent,dirc)+'/'+name[0].split("_")[0],latent_float.detach().cpu())
+                    np.save(os.path.join(args.out_latent,dirc)+'/'+name[0],latent_float.detach().cpu())
+                    
+
+                if args.produce_decompressed_image:
+                    if not(os.path.exists(os.path.join(args.out_decom,dirc))):
+                        os.mkdir(os.path.join(args.out_decom,dirc))
+                    reconstructed = reconstructed.squeeze()
+                    reconstructed = np.asarray(reconstructed*255, dtype=np.uint8)
+                    reconstructed = reconstructed.transpose(1,2,0)
+                    reconstructed = Image.fromarray((reconstructed).astype(np.uint8))
+                    decom_file = os.path.join(os.path.abspath(args.out_decom),dirc,str(args.bit_depth) + '_decom_' + name[0])
+                    reconstructed.save(decom_file)
+
+                if n == args.max_samples:
+                    break
+            if(n==args.max_samples):
+                break
+        raw_bitrate = np.array(bit_rate_raw)
+        cae_bitrate = np.array(bit_rate_cae)
+        os.mkdir(os.path.join(args.bit_dir,dirc))
+        np.save(os.path.join(args.bit_dir,dirc,'raw_bitrate'),raw_bitrate)
+        np.save(os.path.join(args.bit_dir,dirc,'cae_bitrate'),cae_bitrate)
+        
+    # avg_psnr = avg_psnr/(n)
+    # avg_ssim = avg_ssim/(n)
+    avg_cf = np.mean(all_cf)
+    avg_bits = np.mean(all_bits)
+    import json
+    json_content = []
+    json_fillpath = os.path.abspath(args.plot_json)
+    if not os.path.exists(json_fillpath):
+        with open(json_fillpath, 'w') as json_file:
+            json.dump([], json_file)
+
+    with open(json_fillpath, 'r') as json_file:
+        json_content = json.load(json_file)
+
+    # append to the content of json
+    json_content.append({'d': args.n_convblocks,'bit_length':args.bit_depth,'avg_bits':avg_bits,'avg_cf': avg_cf,'Model_name':args.model_file})
+
+    with open(json_fillpath, 'w') as json_file:
+        json.dump(json_content, json_file)
+
+if __name__ == '__main__':
+	import argparse
+
+	parser = argparse.ArgumentParser("""
+	The inference script works on a folder level. Provide this script a folder (--inferdata) with full-scale mammograms which can be created with the dataprep.py script.
+	It also requires a model (--model_file) file to work on which is to be produced by train.py script.
+	All the full-scale mammograms will be processed on by one to collect bpp, ssim and psnr information which will be averaged to produce the numbers to be plotted.
+	
+	To produce bpp-ssim-psnr with varying bit-depth, run the over and over JUST with different --bit_depth. Keep the json filename same.
+
+	(Optionally) Provide a json filename to write the metrics to.
+	(Optionally) You may choose to use AAC (Huffman coding).
+	(Optionally) You may choose to use GPU for all processing.
+	(Optionally) You may choose to produce (--produce_latent_code and --out_latent) the latent code (integer) and the corresponding huffman codebook.
+	(Optionally) You may choose to produce (--produce_decompressed_image and --out_decom) the decompressed images for visualization purpose.
+	(Optionally) You may choose the number of Convolution blocks in the architecture. (CVPR paper have 3)
+		""")
+	parser.add_argument('-m', '--model_file', required=True, type=str, help='Path to the model file')
+	parser.add_argument('--inferdata', type=str, required=True, help='Path to the folder containing full scale mammogram images for inference')
+	parser.add_argument('-t', '--bit_depth', required=True, type=int, help='Required bit depth for Float2Int quantization')
+	parser.add_argument('--gpu', action='store_true', help='Want GPU ?')
+	parser.add_argument('--with_aac', action='store_true', help='Use Adaptive Arithmatic Coding (Huffman coding)')
+	parser.add_argument('-l', '--produce_latent_code', action='store_true', help='Write latent code tensor (integer) as output (possibly along huffman codebook)')
+	parser.add_argument('-d', '--produce_decompressed_image', action='store_true', help='Write decompressed images as output')
+	parser.add_argument('--out_latent', type=str, required=False, default='.', help='Folder to produce the latent codes into ?')
+	parser.add_argument('--out_decom', type=str, required=False, default='.', help='Folder to produce decompressed images into ?')
+	parser.add_argument('-p', '--plot_json', type=str, required=False, default='./plot.json', help='Path for the output json file')
+	parser.add_argument('--n_convblocks', type=int, required=False, default=3, help='no of conv blocks in the architecture')
+	parser.add_argument('-x', '--max_samples', type=int, required=False, default=0, help='limit the number of samples to use for inference')
+	parser.add_argument('--bit_dir' ,type=str,required =False,default='.' ,help='bits array store ?')
+	args = parser.parse_args()
+
+	main( args )
