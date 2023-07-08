cebbb1e7c1e80667c24ca4d684bd0ba3ab2eecd9
PragyadiptaAdhya <118466922+PragyadiptaAdhya@users.noreply.github.com>
2023-07-08 12:41:06 +0530

    Update test.sh

--- src/segmentation/test.sh
+++ src/segmentation/test.sh
@@ -1,10 +1,9 @@
 CUDA_VISIBLE_DEVICES=1 python infer_and_save.py --data_set cityscapes \
---img_dir "/storage/ravi/test_set_5/lat_5_d2" \
---lbl_dir "/storage/ravi/test_set_5/gt" \
+--img_dir "#add path to the saved latent vector spaces of d=2 model" \
+--lbl_dir "# add path to the ground truths" \
 --arch DualSeg_res50 \
 --rgb 1 \
---restore_from "/home/pragyadipta/Semantic-Segmentation-of-Driving-Videos-on-Learning-based-Image-Compression/model_weights_icme/DualSeg_res50_final_d2.pth" \
---output_dir "./test_d2_new_5" \
+--restore_from " add path to the model that you have downloaded named as DualSeg_res50_final_d2.pth or your trained model" \
+--output_dir "#add path to output directory" \
 --num_classes 19 \
 --whole 1 
-# --img_dir /storage/ravik/jpg_vic \
